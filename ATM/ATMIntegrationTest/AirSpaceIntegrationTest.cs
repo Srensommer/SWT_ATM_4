30816134266f48d4639ecd25129b4e91b6b2aa7a
srensommer <srensommer@gmail.com>
2019-10-15 18:36:30 +0200

    Foreslag til overordnet struktur opdateret

---
+++ ATM/ATMIntegrationTest/AirSpaceIntegrationTest.cs
@@ -0,0 +1,28 @@
+﻿using ATM_Application;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMIntegrationTest
+{
+    [TestFixture]
+    class ATMTests_SomethingToTest
+    {
+        private ATM_Placeholder uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new ATM_Placeholder();
+        }
+        //[TestCase()]
+        public void Test_Something()
+        {
+            //Assert.That();
+        }
+    }
+}
