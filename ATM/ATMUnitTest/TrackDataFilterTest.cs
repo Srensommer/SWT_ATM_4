46503242d4d27944b4570fe5a5d37c62e1be1c61
srensommer <srensommer@gmail.com>
2019-10-22 15:17:56 +0200

    TrackDataFilterTest is soon to be good!

--- ATM/ATMUnitTest/TrackDataFilterTest.cs
+++ ATM/ATMUnitTest/TrackDataFilterTest.cs
@@ -5,6 +5,7 @@ using TransponderReceiver;
 using ATM_Application;
 using System.Collections.Generic;
 using System;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,33 +13,34 @@ using System.Threading.Tasks;
 namespace ATMUnitTest
 {
     [TestFixture]
-    class FilterTest
+    public class FilterTest
     {
-        //Dummy data fra Franks pdf.
-        private ITrackDataFilter _trackDataFilter;
         private string dummyTag = "ATR423";
         private int dummyX1 = 200000000;
         private int dummyX2 = 2000;
         private int dummyY = 5000;
         private int dummyAltitude = 2000;
         private DateTime dummyTimestamp = new DateTime(1994, 30, 9, 4, 20, 69);
-
+        TrackData dummyTrackDataAway = new TrackData(dummyTag, dummyX1, dummyY, dummyAltitude, dummyTimestamp);
+        TrackData dummyTrackDataStay = new TrackData(dummyTag, dummyX2, dummyY, dummyAltitude, dummyTimestamp);
+        List<TrackData> dummyData = new List<TrackData>(new List<TrackData> { dummyTrackDataAway, dummyTrackDataStay });
+        List<TrackData> ddAway = new List<TrackData>(new List<TrackData> { dummyTrackDataAway });
         [SetUp]
         public void Setup()
         {
-            _trackDataFilter = new TrackDataFilter();
-            TrackData dummyTrackDataAway = new TrackData(dummyTag, dummyX1, dummyY, dummyAltitude, dummyTimestamp);
-            TrackData dummyTrackDataStay = new TrackData(dummyTag, dummyX2, dummyY, dummyAltitude, dummyTimestamp);
-            List<TrackData> dummyData = new List<TrackData>(new List<TrackData>{ dummyTrackDataAway, dummyTrackDataStay });
+            ITrackDataFilter uut = Substitute.For<TrackDataFilter>();
+
         }
-        [TestCase()]
-        public void FilterTrackAwayTest()
+
+        [TestCase(ddAway)]
+        public void FilterTrackTestRemove(List<TrackData> TestList)
         {
-            //Vis at filteret filtrerer dummyTrackDataAway væk
-        }
-        public void FilterTrackStaysTest()
-        {
-            //Vis at filteret lader dummyTrackDataStay være
+            Assert.IsEmpty(TestList);
         }
     }
 }
+
+ //           List<string> testData = new List<string>();
+  //          testData.Add("ATR423;39045;12932;14000;20151006213456789");
+    //        testData.Add("BCD123;10005;85890;12000;20151006213456789");
+      //      testData.Add("XYZ987;25059;75654;4000;20151006213456789");
