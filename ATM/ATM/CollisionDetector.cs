df076f9e25d0309e78bbe95dbf9e046cb1f03962
Downz <anderesmhandsome@gmail.com>
2019-10-29 23:52:39 +0100

    Bug in Collision fixed + test added

--- ATM/ATM/CollisionDetector.cs
+++ ATM/ATM/CollisionDetector.cs
@@ -6,23 +6,21 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-    class CollisionDetector
+    public class CollisionDetector
     {
         public bool seperationCheck(TrackData track1, TrackData track2)
         {
             //Returnerer true hvis vi skal raise en seperation condition
             if (Math.Abs(track1.Altitude-track2.Altitude) < 300)
             {
-                return true;
-                printToFile();
+                if (Math.Sqrt(Math.Abs(track1.Y - track2.Y) + Math.Abs(track1.X - track2.X)) < 5000)
+                {
+                    printToFile();
+                    return true;
+                }
             }
-            else if (Math.Sqrt(Math.Abs(track1.Y-track2.Y)+Math.Abs(track1.X-track2.X)) < 5000)
-            {
-                return true;
-                printToFile();
-            }
+
             return false;
-            //HUSK at dette skal fjerne den seperation condition vi satte hvis denne tidligere har returneret true!
         }
         private void printToFile()
         {
---
+++ ATM/ATMUnitTest/CollisionDetectorTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM;
+using NUnit.Framework;
+using ATM_Application;
+
+namespace ATMUnitTest
+{
+
+    [TestFixture]
+    class CollisionDetectorTest
+    {
+        private CollisionDetector uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new CollisionDetector();
+
+        }
+        [TestCase()]
+        public void CollisionDetectorNoCollisionAltitude()
+        {
+            TrackData dummyTrackData1 = new TrackData("X1", 10000, 10000, 5000, new DateTime());
+            TrackData dummyTrackData2 = new TrackData("X2", 10000, 10000, 5300, new DateTime());
+            Assert.IsFalse(uut.seperationCheck(dummyTrackData1, dummyTrackData2));
+        }
+
+        [TestCase()]
+        public void CollisionDetectorCollisionAltitude()
+        {
+            TrackData dummyTrackData1 = new TrackData("X1", 10000, 10000, 5000, new DateTime());
+            TrackData dummyTrackData2 = new TrackData("X2", 10000, 10000, 5200, new DateTime());
+            Assert.IsTrue(uut.seperationCheck(dummyTrackData1, dummyTrackData2));
+        }
+
+        [TestCase()]
+        public void CollisionDetectorNoCollisionHorizontal()
+        {
+            TrackData dummyTrackData1 = new TrackData("X1", 12500, 12500, 5000, new DateTime());
+            TrackData dummyTrackData2 = new TrackData("X2", 10000, 10000, 5000, new DateTime());
+            Assert.IsFalse(uut.seperationCheck(dummyTrackData1, dummyTrackData2));
+        }
+
+        [TestCase()]
+        public void CollisionDetectorCollisionHorizontal()
+        {
+            TrackData dummyTrackData1 = new TrackData("X1", 12000, 12000, 5000, new DateTime());
+            TrackData dummyTrackData2 = new TrackData("X2", 10000, 10000, 5000, new DateTime());
+            Assert.IsTrue(uut.seperationCheck(dummyTrackData1, dummyTrackData2));
+        }
+
+    }
+}
