07dcacffc1a32ab5edeaa8331f4fb75bfa614f7c
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-10-31 23:40:27 +0100

    changed architecture regarding collision detector

--- ATM/FlightCalculator.cs
+++ ATM/FlightCalculator.cs
@@ -9,19 +9,16 @@ namespace ATM
 {
     public class FlightCalculator : IFlightCalculator
     {
-        private readonly ICollisionDetector _collisionDetector;
         private readonly IVelocityCalculator _velocityCalculator;
         private readonly IDirectionCalculator _directionCalculator;
-        public FlightCalculator(IVelocityCalculator velocityCalculator, IDirectionCalculator directionCalculator, ICollisionDetector collisionDetector)
+        public FlightCalculator(IVelocityCalculator velocityCalculator, IDirectionCalculator directionCalculator)
         {
-            _collisionDetector = collisionDetector;
             _velocityCalculator = velocityCalculator;
             _directionCalculator = directionCalculator;
         }
 
         public Dictionary<string, FlightData> Calculate(Dictionary<String, FlightData> flightData, List<TrackData> trackData)
         {
-            List<String> collisionList = _collisionDetector.SeperationCheck(trackData);
 
             foreach (TrackData track in trackData)
             {
@@ -29,7 +26,6 @@ namespace ATM
                 {
                     flight.Velocity = _velocityCalculator.CalculateSpeed(flight.CurrentTrackData, track);
                     flight.CompassCourse = _directionCalculator.CalculateDirection(flight.CurrentTrackData, track);
-                    flight.CollisionFlag = collisionList.Contains(flight.Tag);
                     flight.CurrentTrackData = track;
                 }
                 else
