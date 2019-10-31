3a86f046088c9d22b5f209be1632b8f9cf7b6774
Downz <anderesmhandsome@gmail.com>
2019-10-31 11:49:39 +0100

    Collision from 2 elements to list based funcktion + Updated tests

--- ATM/ATM/CollisionDetector.cs
+++ ATM/ATM/CollisionDetector.cs
@@ -8,21 +8,39 @@ namespace ATM
 {
     public class CollisionDetector
     {
-        public bool seperationCheck(TrackData track1, TrackData track2)
+        private List<String> _collisonTagList = new List<string>();
+        public List<String> SeperationCheck(List<TrackData> trackList)
         {
-            //Returnerer true hvis vi skal raise en seperation condition
-            if (Math.Abs(track1.Altitude-track2.Altitude) < 300)
+            List<String> collisionList = new List<String>();
+
+            foreach (TrackData track1 in trackList)
             {
-                if (Math.Sqrt(Math.Abs(track1.Y - track2.Y) + Math.Abs(track1.X - track2.X)) < 5000)
+                foreach (TrackData track2 in trackList)
                 {
-                    printToFile();
-                    return true;
+                    if (Math.Sqrt(Math.Abs(track1.Y - track2.Y) + Math.Abs(track1.X - track2.X)) < 5000)
+                    {
+                        if (Math.Abs(track1.Altitude - track2.Altitude) < 300)
+                        {
+                            if (track1 != track2)
+                            {
+                                if (!((_collisonTagList.Contains(track1.Tag) && _collisonTagList.Contains(track2.Tag) ||
+                                       (collisionList.Contains(track1.Tag) && collisionList.Contains(track2.Tag)))))
+                                {
+                                    collisionList.Add(track1.Tag);
+                                    collisionList.Add(track2.Tag);
+                                    printToFile(track1, track2);
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
-            return false;
+            _collisonTagList = collisionList;
+            
+            return collisionList;
         }
-        private void printToFile()
+        private void printToFile(TrackData track1, TrackData track2)
         {
         //Log hvornår seperation condition skete. Dette skal indeholde tid for sep con. og tag for de pågældende fly
         //Måske den her i virkeligheden skal have sin egen klasse..
