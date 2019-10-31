84c8dcbd5b0c1788d851140c429b340da527d107
Downz <anderesmhandsome@gmail.com>
2019-10-31 15:51:02 +0100

    Project files

---
+++ ATM/ATMUnitTest/FlightCalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM;
+using NUnit.Framework;
+
+namespace ATMUnitTest
+{
+
+    [TestFixture]
+    class FLightCalculatorTest
+    {
+        private IFlightCalculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new FlightCalculator();
+        }
+        [Test]
+        public void CollisionDetectorNoCollisionVerticalDistance()
+        {
+            Dictionary<String, FlightData> flightData = new Dictionary<String, FlightData>();
+            List<TrackData> trackData = new List<TrackData>();
+
+
+            flightData =  uut.Calculate(flightData, trackData);
+
+        }
+
+
+    }
+}
