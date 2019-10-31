83b7acc648beb396ee561ea2385d7794f7ae525a
Downz <anderesmhandsome@gmail.com>
2019-10-31 23:05:51 +0100

    Bail out insted of nested if-statements

--- ATM/CollisionDetector.cs
+++ ATM/CollisionDetector.cs
@@ -29,22 +29,16 @@ namespace ATM
             {
                 foreach (TrackData track2 in trackList)
                 {
-                    if (Math.Sqrt(Math.Abs(track1.Y - track2.Y) + Math.Abs(track1.X - track2.X)) < 5000)
-                    {
-                        if (Math.Abs(track1.Altitude - track2.Altitude) < 300)
-                        {
-                            if (track1 != track2)
-                            {
-                                if (!((_collisonTagList.Contains(track1.Tag) && _collisonTagList.Contains(track2.Tag) ||
-                                       (collisionList.Contains(track1.Tag) && collisionList.Contains(track2.Tag)))))
-                                {
-                                    collisionList.Add(track1.Tag);
-                                    collisionList.Add(track2.Tag);
-                                    printToFile(track1, track2);
-                                }
-                            }
-                        }
-                    }
+                    if (!(Math.Sqrt(Math.Abs(track1.Y - track2.Y) + Math.Abs(track1.X - track2.X)) < 5000)) continue;
+                    if (!(Math.Abs(track1.Altitude - track2.Altitude) < 300)) continue;
+                    if (track1 == track2) continue;
+                    if ((_collisonTagList.Contains(track1.Tag) && _collisonTagList.Contains(track2.Tag) ||
+                         (collisionList.Contains(track1.Tag) && collisionList.Contains(track2.Tag))))
+                        continue;
+                                
+                    collisionList.Add(track1.Tag);
+                    collisionList.Add(track2.Tag);
+                    GenerateConditionString(track1, track2);
                 }
             }
 
@@ -53,10 +47,11 @@ namespace ATM
             return collisionList;
         }
 
-        private void printToFile(TrackData track1, TrackData track2)
+        private String GenerateConditionString(TrackData track1, TrackData track2)
         {
-            string appendText = "Time of occurrence: " + new DateTime() + ", Tags: " + track1.Tag + ", " + track2.Tag + Environment.NewLine;
+            string appendText = "Time of occurrence: " + track1.Timestamp + ", Tags: " + track1.Tag + ", " + track2.Tag + Environment.NewLine;
             File.AppendAllText(path, appendText);
+            return appendText;
         }
 
     }
